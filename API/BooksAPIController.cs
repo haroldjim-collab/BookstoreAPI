61d7cdaa8ed01a73e26f8f5c37361513f50a5c4e
haroldjim <jimnycrypto@yahoo.com>
2022-12-10 00:41:30 +0800

    feature 1

--- API/BooksAPIController.cs
+++ API/BooksAPIController.cs
@@ -51,6 +51,8 @@ namespace BookstoreAPI.API
             //save to db
             int booksId = 12; // await bookServices.CreateBookAsync(_mapper.Map<Books>(books));
 
+            //comment feature-1
+
             return booksId;
         }
 
